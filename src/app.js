7ccdc2a1bd4eaca90c16162e24328e96cd27a58d
Anubhavjain1201 <anubhav.jain1201@gmail.com>
2025-12-22 17:48:32 +0530

    some updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { customErrorHandler } from "./middewares/customErrorHandler.js"
+import { customErrorHandler } from "./middlewares/customErrorHandler.middlewares.js"
 
 const app = express()
 

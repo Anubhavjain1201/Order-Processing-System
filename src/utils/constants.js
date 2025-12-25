f2659804fb2fabcd9221894e237ae6eb84f3e401
Anubhavjain1201 <anubhav.jain1201@gmail.com>
2025-12-25 14:46:04 +0530

    add some comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,7 @@
+// issuer of identity tokens
 const TOKEN_ISSUER = "identity.orderproc.com"
+
+// Types of scopes for identity tokens
 const SCOPES = {
     REFRESH: "refresh",
     ACCESS_ORDER: "orders"

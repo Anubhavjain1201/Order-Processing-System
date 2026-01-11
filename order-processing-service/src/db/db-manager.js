7741c076f94ce7a1ea1da1eadfb5a05d1f065dc2
Anubhavjain1201 <anubhav.jain1201@gmail.com>
2026-01-12 00:18:00 +0530

    basic setup for order-processing-service

---
+++ order-processing-service/src/db/db-manager.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+async function connectDb() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            maxPoolSize: process.env.MONGO_MAX_POOL_SIZE || 100,
+            minPoolSize: process.env.MONGO_MIN_POOL_SIZE || 10
+        })
+        console.log("MongoDB connected successfully")
+    } catch (error) {
+        console.error(`Error occurred while connecting to MongoDB: ${error}`)
+    }
+}
+
+export { connectDb }
